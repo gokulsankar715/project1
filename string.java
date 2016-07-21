89717df3649220d7e2cf75bd9bb33056a6af27b8
gokulsankary <gokuly437@gmail.com>
2016-07-21 18:33:30 +0530

    string.java

---
+++ string.java
@@ -0,0 +1,8 @@
+public class str{
+public static void main(String[]args){
+Scanner s=new Scanner(System.in);
+String a=s.nextLine();
+StringBuffer b=a.toString();
+system.out.println(b.reverse());
+}
+}
