667bbc43e6bcee35dd8b2b1d1b5e5ce62d3f1e1d
gokulsankary <gokuly437@gmail.com>
2016-07-21 18:20:46 +0530

    Create sankar.java
    factorial

---
+++ sankar.java
@@ -0,0 +1,18 @@
+public class fact{
+public static void main(String[]args){
+ Scanner s=new Scanner(System.in);
+ a=s.nextInt();
+ int b,c=1;
+ if(b<0)
+ {
+ system.out.println(c);
+ }
+ else
+ {
+ for(b=1;b<=a;b++)
+ {
+ c=c*b;
+ }
+ system.out.println(c);}
+ }
+ }
